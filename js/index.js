41a8a5f390c3cdfb1dd17eb84d545d8f989d2ede
uployalty <slimpaul@outlook.com>
2019-03-05 14:20:48 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,52 @@
+function showMenu(){
+	$("#header-right-menu").toggle(500);
+}
+/*function tiao1(){
+	$("#container1").show();
+	$("#container2").hide();
+	$("#container3").hide();
+	$("#container4").hide();
+}
+function tiao2(){
+	$("#container1").hide();
+	$("#container2").show();
+	$("#container3").hide();
+	$("#container4").hide();
+}
+function tiao3(){
+	$("#container1").hide();
+	$("#container2").hide();
+	$("#container3").show();
+	$("#container4").hide();
+}
+function tiao4(){
+	$("#container1").hide();
+	$("#container2").hide();
+	$("#container3").hide();
+	$("#container4").show();
+}*/
+/*function tiao2(){
+	$("#chat").hide();
+	$("#chat3").hide();
+	$("#chat4").hide();
+	$("#chat2").show();
+}
+function tiao1(){
+	$("#chat").show();
+	$("#chat3").hide();
+	$("#chat4").hide();
+	$("#chat2").hide();
+}
+function tiao3(){
+	$("#chat").hide();
+	$("#chat3").show();
+	$("#chat4").hide();
+	$("#chat2").hide();
+}
+function tiao4(){
+	$("#chat").hide();
+	$("#chat3").hide();
+	$("#chat4").show();
+	$("#chat2").hide();
+}*/
+
